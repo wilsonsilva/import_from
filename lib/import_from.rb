35b7927ef60f6fb612b6dc8cd01d6fb8250ac735
Wilson Silva <wilson.dsigns@gmail.com>
2025-12-28 21:36:53 +0800

    Set RUBY_BOX env variable if Ruby::Box is disabled
    Replaces the exception with setting ENV['RUBY_BOX'] to '1' when Ruby::Box is not enabled, allowing the code to proceed instead of raising an error.

--- lib/import_from.rb
+++ lib/import_from.rb
@@ -3,7 +3,7 @@
 require_relative 'import_from/version'
 require_relative 'import_from/core_ext/kernel'
 
-raise 'Ruby::Box is disabled. Set RUBY_BOX=1' unless Ruby::Box.enabled?
+ENV['RUBY_BOX'] = '1' unless Ruby::Box.enabled?
 
 # Entry point
 module ImportFrom
